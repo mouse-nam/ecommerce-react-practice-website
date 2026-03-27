f1f9d2295c563c480188f7fec745e30d1b8a08e2
naznin <mitu.bauet@gmail.com>
2026-03-27 15:02:03 +0600

    feat: useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,31 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
+function GreetingsV0() {
+  return <h1>Hello, I am Naznin, I am 29 years old.</h1>
+}
+
+function GreetingsWithPropsV1(props) {
+  return <h1>Hello, I am {props.name}, I am {props.age} years old.</h1>
+}
+
+function Greetings({name, age}) {
+  return <h1>Hello, I am {name}, I am {age} years old.</h1>
+}
+
 function App() {
   const name = "Naznin"
   const age = 29
 
+  const [showGreetings, setShowGreetings] = useState(false)
+  function toggleGreetings() {
+    setShowGreetings(!showGreetings)
+  }
+
   return (
     <div>
-      <h1>Hello {name}</h1>
-      <p>A {age} years old SWE aspirant.</p>
+      <button onClick={toggleGreetings}>Click me!</button>
+      {showGreetings && <Greetings name="Mitu" age="30" /> }
     </div>
   )
 }

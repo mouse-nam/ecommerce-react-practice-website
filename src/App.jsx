acbc1a825fe0d9fc0661c479f3c2280042387e5d
Ahsan Habib <shehanuk.ahsan@gmail.com>
2026-03-29 10:11:55 +0600

    fix: starter files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,4 @@
-import { useState } from "react";
-import { Link, Route, Routes } from "react-router-dom";
-import HomePage from "./examples/components/HomePage";
-import AboutPage from "./examples/components/AboutPage";
-import LoginPage from "./examples/components/LoginPage";
-import ProfilePage from "./examples/components/ProfilePage";
-import Navbar from "./examples/components/NavBar";
-import { AuthContext } from "./AuthContext";
+
 
 function App() {
   return (

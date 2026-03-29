3b97b978959131819c28fe62d852e786a405c97a
Ahsan Habib <shehanuk.ahsan@gmail.com>
2026-03-29 14:50:53 +0600

    feat: home ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,23 @@
+import { Link } from "react-router-dom";
+import { getProducts } from "../data/products";
+import ProductCard from "../components/ProductCard";
+
 export default function Home() {
-    return <div>Home Page</div>
+  const products = getProducts();
+  return (
+    <div className="page">
+      <div className="home-hero">
+        <h1 className="home-title">Welcome to ShopUp</h1>
+        <p className="home-subtitle">A react mock eCommerce web app.</p>
+      </div>
+      <div className="container">
+        <h2 className="page-title">Our Products</h2>
+        <div className="product-grid">
+          {products.map((product) => (
+            <ProductCard product={product} key={product.id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }

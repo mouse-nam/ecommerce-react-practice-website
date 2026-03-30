7e3764222029f10c3a47ff5cd2310139ab1662c3
Ahsan Habib <shehanuk.ahsan@gmail.com>
2026-03-30 11:27:48 +0600

    feat: auth form validation

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function Auth() {
   const [mode, setMode] = useState("signup");
-  console.log(mode);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  function onSubmit() {
+    alert("Submiteed");
+  }
+
   return (
     <div className="page">
       <div className="container">
@@ -10,20 +20,49 @@ export default function Auth() {
           <h1 className="page-title">
             {mode === "signup" ? "Sign Up" : "Login"}
           </h1>
-          <form className="auth-form">
+          <form className="auth-form" onSubmit={handleSubmit(onSubmit)}>
             <div className="form-group">
               <label htmlFor="email" className="form-label">
                 Email
               </label>
-              <input type="email" className="form-input" id="email" />
+              <input
+                type="email"
+                className="form-input"
+                id="email"
+                {...register("email", { required: "Email is required" })}
+              />
+              {errors.email && (
+                <span className="form-error">{errors.email.message}</span>
+              )}
             </div>
             <div className="form-group">
               <label htmlFor="password" className="form-label">
                 Password
               </label>
-              <input type="password" className="form-input" id="password" />
+              <input
+                type="password"
+                className="form-input"
+                id="password"
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 3,
+                    message: "Password must be at least 3 character long.",
+                  },
+                  maxLength: {
+                    value: 6,
+                    message: "Password must be less than 7 character long.",
+                  },
+                })}
+              />
+              {errors.password && (
+                <span className="form-error">{errors.password.message}</span>
+              )}
             </div>
-            <button className="btn btn-primary btn-large">Sign Up</button>
+
+            <button className="btn btn-primary btn-large">
+              {mode === "signup" ? "Sign Up" : "Login"}
+            </button>
           </form>
 
           <div className="auth-switch">

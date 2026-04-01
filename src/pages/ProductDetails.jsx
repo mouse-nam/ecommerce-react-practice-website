fb089e19c2af1bbb5904c6a5fbf019333f6ef804
Ahsan Habib <shehanuk.ahsan@gmail.com>
2026-04-01 12:04:30 +0600

    feat: add to cart in product detail page

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { getProductById } from "../data/products";
-// import { useCart } from "../context/CartContext";
+import { useCart } from "../context/CartContext";
 
 export default function ProductDetails() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
   const navigate = useNavigate();
-  //   const { addToCart, cartItems } = useCart();
+  const { addToCart, cartItems } = useCart();
 
   useEffect(() => {
     const foundProduct = getProductById(id);
@@ -24,11 +24,11 @@ export default function ProductDetails() {
     return <h1>Loading...</h1>;
   }
 
-  //   const productInCart = cartItems.find((item) => item.id === product.id);
+  const productInCart = cartItems.find((item) => item.id === product.id);
 
-  //   const productQuantityLabel = productInCart
-  //     ? `(${productInCart.quantity})`
-  //     : "";
+  const productQuantityLabel = productInCart
+    ? `(${productInCart.quantity})`
+    : "";
 
   return (
     <div className="page">
@@ -43,9 +43,9 @@ export default function ProductDetails() {
             <p className="product-detail-description">{product.description}</p>
             <button
               className="btn btn-primary"
-              //   onClick={() => addToCart(product.id)}
+              onClick={() => addToCart(product.id)}
             >
-              Add to Cart
+              Add to Cart {productQuantityLabel}
             </button>
           </div>
         </div>
